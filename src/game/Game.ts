4bf3a3895a8d0329c405acacc499a164036611f2
Doramanjyu <24859285+Doramanjyu@users.noreply.github.com>
2023-12-12 00:33:31 +0900

    Jump sample (#5)

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -5,10 +5,12 @@ import spriteUrl from './sprite.png'
 class Game {
   readonly ctx: CanvasRenderingContext2D
   readonly pc: Anime
+  readonly pc_jump: Anime
 
   dir: number
+  state: number
+  mush_size: number
   loaded?: boolean
-  cnt: number
 
   constructor(canvas: HTMLCanvasElement) {
     const ctx = canvas.getContext('2d')
@@ -31,12 +33,18 @@ class Game {
     )
     sprite.src = spriteUrl
 
-    this.cnt = 0
     this.dir = 0
+    this.mush_size = 2
+    this.state = 0
     this.pc = new Anime(sprite, {
       topLeft: [0, 0],
       sz: [40, 32],
       frames: [0, 1, 0, 0, 0, 0, 2, 3, 4, 5, 4, 4, 3, 2],
+    })
+    this.pc_jump = new Anime(sprite, {
+      topLeft: [240, 0],
+      sz: [40, 32],
+      frames: [0, 1, 2, 3, 4, 5],
     })
   }
 
@@ -52,6 +60,17 @@ class Game {
         case 'ArrowRight':
           this.dir = 3
           break
+        case 'ArrowUp':
+          this.mush_size = this.mush_size < 2 ? this.mush_size + 1 : 2
+          break
+        case 'ArrowDown':
+          this.mush_size = this.mush_size >= 0 ? this.mush_size - 1 : 0
+          break
+        case 'Space':
+          if (this.state === 0) {
+            this.state = 1
+          }
+          break
       }
     }
     const keyup = (e: KeyboardEvent) => {
@@ -62,6 +81,11 @@ class Game {
           break
         case 'ArrowRight':
           this.dir = 0
+          break
+        case 'Space':
+          if (this.state === 1) {
+            this.state = 2
+          }
           break
       }
     }
@@ -83,15 +107,38 @@ class Game {
       return
     }
 
-    this.cnt++
-    const mushPat = [0, 1, 2, 1]
-    const mushSize = mushPat[Math.floor(this.cnt / 50) % 4]
-
-    this.pc.tick()
-    this.pc.draw(this.ctx, [140, 128], 2, 0, 0)
-    this.pc.draw(this.ctx, [140 + this.dir, 128], 2, 0, 1)
-    if (mushSize > 0) {
-      this.pc.draw(this.ctx, [140, 128], 2, 0, mushSize + 1)
+    switch (this.state) {
+      case 0:
+      case 1:
+        this.pc.draw(this.ctx, [140, 128], 2, 0, 0)
+        this.pc.draw(this.ctx, [140 + this.dir, 128], 2, 0, 1)
+        if (this.mush_size > 0) {
+          this.pc.draw(this.ctx, [140, 128], 2, 0, this.mush_size + 1)
+        }
+        break
+      case 2: {
+        const y = [0, -8, -12, -16, -12, -8, 0][this.pc_jump.frame]
+        this.pc_jump.draw(this.ctx, [140, 128 + y], 2, 0, 0)
+        this.pc_jump.draw(this.ctx, [140, 128 + y], 2, 0, 1)
+        if (this.mush_size > 0) {
+          this.pc_jump.draw(this.ctx, [140, 128 + y], 2, 0, this.mush_size + 1)
+        }
+        break
+      }
+    }
+    switch (this.state) {
+      case 0:
+        this.pc.tick()
+        break
+      case 1:
+        this.pc.tick(10)
+        this.pc_jump.tick(0)
+        break
+      case 2:
+        if (this.pc_jump.tick() === 0) {
+          this.state = 0
+        }
+        break
     }
   }
 }
