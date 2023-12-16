e6ec372bd0b3b6bbc650545358e4eb2390d90aaa
Doramanjyu <24859285+Doramanjyu@users.noreply.github.com>
2023-12-16 18:01:42 +0900

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import path from 'path'
-
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tsconfigPaths()],
-    resolve: {
-      alias: {
-        '@doramanjyu/purin-game': path.resolve(__dirname, 'src'),
-      },
-    },
+    plugins: [
+      react(),
+      tsconfigPaths({
+        root: './',
+      }),
+    ],
     server: {
       port: 8080,
       strictPort: true,
